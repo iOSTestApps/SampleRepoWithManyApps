6e56b9067e61f83d79047d181b54b12a86309e69
Romain Pouclet <romain.pouclet@gmail.com>
2019-01-21 10:47:55 -0800

    Add a bunch of sample apps

---
+++ App-2/App-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App-2
+//
+//  Created by Romain Pouclet on 2019-01-21.
+//  Copyright © 2019 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SampleAppWithEscapedPods/SampleAppWithEscapedPods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAppWithEscapedPods
+//
+//  Created by Romain Pouclet on 2019-01-15.
+//  Copyright © 2019 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
